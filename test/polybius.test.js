68fc10540b1c564fff57f1a93f13a6616bddef16
Rafael Villa <villalugo96@gmail.com>
2022-02-19 02:21:49 -0500

    fixed vscode polybius

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -3,32 +3,32 @@ const expect = require('chai').expect;
 
 describe ('Polybius', () => {
     it("thinkful", () => {
-        const actual = polybius("thinkful")
-        const expected = "4432423352125413"
-        expect(actual).to.equal(expected)
-    })
+        const actual = polybius("thinkful");
+        const expected = "4432423352125413";
+        expect(actual).to.eql(expected);
+    });
 
-    it("Hello World", () => {
-        const actual = polybius("Hello World")
-        const expected = "32511313432543241341"
-        expect(actual).to.equal(expected)
-    })
+    it("Hello world", () => {
+        const actual = polybius("Hello world");
+        const expected = "3251131343 2543241341";
+        expect(actual).to.eql(expected);
+    });
 
     it("3251131343 2543241341", () => {
-        const actual = polybius("32511313432543241341", false)
-        const expected = "hello world"
-        expect(actual).to.equal(expected)
-    })
+        const actual = polybius("3251131343 2543241341", false);
+        const expected = "hello world";
+        expect(actual).to.eql(expected);
+    });
 
     it("4432423352125413", () => {
-        const actual = polybius("4432423352125413", false)
-        const expected = "th(i/j)nkful"
-        expect(actual).to.equal(expected)
-    })
+        const actual = polybius("4432423352125413", false);
+        const expected = "th(i/j)nkful";
+        expect(actual).to.eql(expected);
+    });
 
     it("44324233521254134", () => {
-        const actual = polybius("44324233521254134", false)
-        const expected = "false"
-        expect(actual).to.equal(expected) 
+        const actual = polybius("44324233521254134", false);
+        const expected = false
+        expect(actual).to.eql(expected) ;
     })
 });
